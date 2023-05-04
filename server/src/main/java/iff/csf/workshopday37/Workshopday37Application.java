9d6c2438cd859ad86be219db14d82fa44894ac37
Wang Guangzu <wgz199154@hotmail.com>
2023-05-04 23:42:00 +0800

    initial commit

---
+++ server/src/main/java/iff/csf/workshopday37/Workshopday37Application.java
@@ -0,0 +1,13 @@
+package iff.csf.workshopday37;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshopday37Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workshopday37Application.class, args);
+	}
+
+}
---
+++ server/src/test/java/iff/csf/workshopday37/Workshopday37ApplicationTests.java
@@ -0,0 +1,13 @@
+package iff.csf.workshopday37;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshopday37ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
